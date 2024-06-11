b6eb75f4880ed0c33a38a152f03a77b6d0447226
kimyu0218 <serena35@ewhain.net>
2024-06-12 02:35:48 +0900

    Style: goole-java-formatter 적용

--- hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -10,31 +10,33 @@ import java.util.Optional;
 
 @Service
 public class MemberService {
-    private final MemberRepository memberRepository;
+  private final MemberRepository memberRepository;
 
-    @Autowired
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+  @Autowired
+  public MemberService(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
+  }
 
-    public Long join(Member member) {
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
-    }
+  public Long join(Member member) {
+    validateDuplicateMember(member);
+    memberRepository.save(member);
+    return member.getId();
+  }
 
-    public List<Member> findMembers() {
-        return memberRepository.findAll();
-    }
+  public List<Member> findMembers() {
+    return memberRepository.findAll();
+  }
 
-    public Optional<Member> findOne(Long memberId) {
-        return memberRepository.findById(memberId);
-    }
+  public Optional<Member> findOne(Long memberId) {
+    return memberRepository.findById(memberId);
+  }
 
-    private void validateDuplicateMember(Member member) {
-        memberRepository.findByName(member.getName())
-                .ifPresent(m -> {
-                    throw new IllegalStateException("이미 존재하는 회원입니다.");
-                });
-    }
+  private void validateDuplicateMember(Member member) {
+    memberRepository
+        .findByName(member.getName())
+        .ifPresent(
+            m -> {
+              throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+  }
 }
--- hello-spring/src/test/java/hello/hello_spring/HelloSpringApplicationTests.java
+++ hello-spring/src/test/java/hello/hello_spring/HelloSpringApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HelloSpringApplicationTests {
-
   @Test
   void contextLoads() {}
 }
