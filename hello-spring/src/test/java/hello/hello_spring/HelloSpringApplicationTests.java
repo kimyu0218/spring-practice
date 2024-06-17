cb884076110b37fed6c28194bd592d83550f0fab
kimyu0218 <serena35@ewhain.net>
2024-06-17 16:08:31 +0900

    Refactor: 테스트 어노테이션 수정
    - `@TestPropertySource` 삭제
    - 대신 `@SpringBootTest`에 `spring.config.location`으로 작성

--- hello-spring/src/test/java/hello/hello_spring/HelloSpringApplicationTests.java
+++ hello-spring/src/test/java/hello/hello_spring/HelloSpringApplicationTests.java
@@ -3,14 +3,28 @@ package hello.hello_spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.properties")
+@SpringBootTest(properties = "spring.config.location=classpath:application-test.properties")
 @ContextConfiguration(classes = SpringTestConfig.class)
 class HelloSpringApplicationTests {
 
   @Test
   void contextLoads() {
   }
+
+//  @TestConfiguration
+//  static class SpringTestConfig {
+//
+//    private final MemberRepository memberRepository;
+//
+//    @Autowired
+//    public SpringTestConfig(MemberRepository memberRepository) {
+//      this.memberRepository = memberRepository;
+//    }
+//
+//    @Bean
+//    public MemberService memberService() {
+//      return new MemberService(memberRepository);
+//    }
+//  }
 }
