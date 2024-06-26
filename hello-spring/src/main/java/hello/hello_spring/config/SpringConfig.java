5e82237ca04f82df8d8cd28a8d69705193c568f7
kimyu0218 <serena35@ewhain.net>
2024-06-26 18:48:16 +0900

    Refactor: 설정 파일 분리

--- hello-spring/src/main/java/hello/hello_spring/config/SpringConfig.java
+++ hello-spring/src/main/java/hello/hello_spring/config/SpringConfig.java
@@ -1,52 +1,15 @@
 package hello.hello_spring.config;
 
-import hello.hello_spring.repository.MemberRepository;
-import hello.hello_spring.service.MemberService;
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(MemberConfig.class)
 public class SpringConfig {
-
-//  private final DataSource dataSource;
-//
-//  @Autowired
-//  public SpringConfig(DataSource dataSource) {
-//    this.dataSource = dataSource;
-//  }
-
-//  private final EntityManager em;
-//
-//  @Autowired
-//  public SpringConfig(EntityManager em) {
-//    this.em = em;
-//  }
-
-  private final MemberRepository memberRepository;
-
-  @Autowired
-  public SpringConfig(MemberRepository memberRepository) {
-    this.memberRepository = memberRepository;
-  }
 
   @PostConstruct
   void postConstruct() {
     System.out.println("POST CONSTRUCT : " + this.getClass().getName());
   }
-
-  @Bean
-  public MemberService memberService() {
-//    return new MemberService(memberRepository());
-    return new MemberService(memberRepository);
-  }
-
-//  @Bean
-//  public MemberRepository memberRepository() {
-//    return new MemoryMemberRepository();
-//    return new JdbcMemberRepository(dataSource);
-//    return new JdbcTemplateMemberRepository(dataSource);
-//    return new JpaMemberRepository(em);
-//  }
 }
