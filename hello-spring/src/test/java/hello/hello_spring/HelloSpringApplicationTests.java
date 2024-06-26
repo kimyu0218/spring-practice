708697e6b060fe54b1c84248d9a59af2815ed576
kimyu0218 <serena35@ewhain.net>
2024-06-26 19:01:29 +0900

    Remove: 불필요한 프로퍼티 삭제

--- hello-spring/src/test/java/hello/hello_spring/HelloSpringApplicationTests.java
+++ hello-spring/src/test/java/hello/hello_spring/HelloSpringApplicationTests.java
@@ -1,12 +1,9 @@
 package hello.hello_spring;
 
-import hello.hello_spring.config.SpringIntegrationTestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = SpringIntegrationTestConfig.class, properties = "spring.config.location=classpath:application-test.properties")
-@ActiveProfiles("integration")
+@SpringBootTest(properties = "spring.config.location=classpath:application-test.properties")
 class HelloSpringApplicationTests {
 
   @Test
